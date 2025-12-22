ca4d54f48433ffe9381894e00adff3978fc20761
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-12-22 16:15:07 -0300

    infra: configure webauthn allowed origins in `production`

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.webauthn_origin = "https://devise-webauthn.cedarcode.com"
 end
