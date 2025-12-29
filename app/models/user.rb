f46a9904bfc8280c6d774a89fdeca67adc0e6b13
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-12-29 18:24:18 -0300

    chore(`standard`): fix `Layout/ArgumentAlignment` offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable,
-         :passkey_authenticatable, :webauthn_two_factor_authenticatable,
-         authentication_keys: [:username]
+    :passkey_authenticatable, :webauthn_two_factor_authenticatable,
+    authentication_keys: [:username]
 
   PASSWORD_LENGTH = 6..128
 
